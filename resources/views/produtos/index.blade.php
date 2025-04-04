2e3fd7cbc803cc8a034d92fb92f13cc6167f03ce
marquiiis <mr_ric@hotmail.com>
2025-04-04 13:23:14 -0300

    Eric - alteração layout produtos

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -34,9 +34,54 @@
                     <td>R$ {{ number_format($produto->precomax, 2, ',', '.') }}</td>
                     <td>{{ $produto->estoque }}</td>
                     <td class="text-end">
-                        <a href="{{ route('produtos.edit', $produto->id) }}" class="btn btn-sm btn-outline-secondary me-1" title="Editar">
+                        <!-- Botão de Editar -->
+                        <button class="btn btn-sm btn-outline-secondary me-1" data-bs-toggle="modal" data-bs-target="#editarProdutoModal-{{ $produto->id }}">
                             <i class="fas fa-pen"></i>
-                        </a>
+                        </button>
+                        <!-- Modal de Edição -->
+                        <div class="modal fade" id="editarProdutoModal-{{ $produto->id }}" tabindex="-1" aria-labelledby="editarProdutoLabel-{{ $produto->id }}" aria-hidden="true">
+                          <div class="modal-dialog modal-lg">
+                            <form method="POST" action="{{ route('produtos.update', $produto->id) }}">
+                                @csrf
+                                @method('PUT')
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="editarProdutoLabel-{{ $produto->id }}">Editar Produto</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="row">
+                                            <div class="col-md-6 mb-2">
+                                                <label>Código Interno</label>
+                                                <input type="text" name="codigointerno" class="form-control" value="{{ $produto->codigointerno }}">
+                                            </div>
+                                            <div class="col-md-6 mb-2">
+                                                <label>Descrição</label>
+                                                <input type="text" name="descr" class="form-control" value="{{ $produto->descr }}">
+                                            </div>
+                                            <div class="col-md-6 mb-2">
+                                                <label>Marca</label>
+                                                <input type="text" name="marca" class="form-control" value="{{ $produto->marca }}">
+                                            </div>
+                                            <div class="col-md-6 mb-2">
+                                                <label>Preço Máx</label>
+                                                <input type="number" step="0.01" name="precomax" class="form-control" value="{{ $produto->precomax }}">
+                                            </div>
+                                            <div class="col-md-6 mb-2">
+                                                <label>Estoque</label>
+                                                <input type="number" name="estoque" class="form-control" value="{{ $produto->estoque }}">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">Salvar</button>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                    </div>
+                                </div>
+                            </form>
+                          </div>
+                        </div>
+
                         <form action="{{ route('produtos.destroy', $produto->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Remover este produto?')">
                             @csrf @method('DELETE')
                             <button class="btn btn-sm btn-outline-danger" title="Excluir">
